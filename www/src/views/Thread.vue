c50eac594a2da37994aedf0508f41ec80f1f2957
also <wuzhyy@163.com>
2017-06-04 19:48:19 +0800

    bk

--- www/src/views/Thread.vue
+++ www/src/views/Thread.vue
@@ -38,33 +38,66 @@
 
   <hr>
   <div class="posts">
-    <div class="post" v-for="(post, index) in posts"> 
-      <div class="poster media">
-        <figure class="media-left">
-          <p class="image is-32x32"> <img :src="post.poster.face_url"> </p>
-        </figure>
-        <div class="media-content">
-          <div class="content">
-            <h4> {{ post.poster.id }}</h4>
-            <small> {{ post.time }}</small>
-          </div>
-        </div>
-        <div class="media-right">
-          <span>{{ index == 0 ? '沙发' : index == 1 ? '板凳' : index + 1 + '楼' }}</span>
+    <vue-recyclist
+    :list="posts"
+    :size="recyclist.size"
+    :tombstone="true"
+    :loadmore="nextPage">
+    <template slot="tombstone" scope="props">
+    <div class="item tombstone">
+      <div class="avatar"></div>
+      <div class="bubble">
+        <p></p>
+        <p></p>
+        <p></p>
+        <div class="meta">
+          <time class="posted-date"></time>
         </div>
       </div>
-      <div class="article-body content" v-html="post.content"> </div>
-      <hr>
     </div>
+    </template>
+      <template slot="item" scope="props">
+      <div :id="props.data.id"> 
+        <div class="poster media">
+          <figure class="media-left">
+          <p class="image is-32x32"> <img :src="props.data.poster ? props.data.poster.face_url : ''"> </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <h4> {{ props.data.poster ? props.data.poster.id : '已注销'}}</h4>
+              <small> {{ props.data.time }}</small>
+            </div>
+          </div>
+          <div class="media-right">
+            <span>{{ props.index == 0 ? '沙发' : props.index == 1 ? '板凳' : props.index + 1 + '楼' }}</span>
+          </div>
+        </div>
+        <div class="article-body content" v-html="props.data.content"> </div>
+        <hr>
+      </div>
+      </template>
+    </vue-recyclist>
+
   </div>
 </div>
 </template>
 <script>
 import * as api from 'api/thread'
 
+import VueRecyclist from 'vue-recyclist'
+
 export default {
   data () {
     return {
+      query: {
+        board: '',
+        id: ''
+      },
+      recyclist: {
+        size: 9,
+        spinner: true,
+        tombstone: !+localStorage['tombstone']
+      },
       title: '加载中...',
       time: '',
       gid: 0,
@@ -72,39 +105,60 @@ export default {
         description: 'loading',
         name: '...'
       },
+      lastPage: 0,
       anony: false,
       mainPost: null,
       posts:  []
     }
   },
 
+  components: {
+    VueRecyclist
+  },
+
+  watch: {
+    tombstone(val) {
+      localStorage['tombstone'] = +!val;
+      this.id = 0;
+      this.list = [];
+      this.loadmore();
+    }
+  },
+
   created() {
-    this.fetchArticles(this.$route.params, this.$route.query);
+    this.query = this.$route.params;
+    //this.fetchArticles(this.query, this.$route.query);
   },
 
   methods: {
-    fetchArticles({board, id}, params) {
-      api.getThread(board, id, params).then((res) => {
-        if (res.success) {
-          const data = res.data;
+    nextPage() {
+      const page = ++ this.lastPage;
+      api.getThread(this.query.board, this.query.id, { page }).then((res) => {
+        if (! res.success) {
+          // TODO
+          return false;
+        }
+
+        const data = res.data;
+        if (page == 1) {
           this.title = data.title;
           this.time = data.time;
           this.gid = data.gid;
           this.board = data.board;
           this.anony = data.anony;
-          if (! data.articles.length) {
-            // TODO
-          }
           if (data.articles[0]['id'] == this.gid) {
             [this.mainPost, ...this.posts] = data.articles;
+            //this.recyclist.size = data.articles.length - 1;
           } else {
             this.posts = data.articles;
           }
         } else {
-          // TODO
+          this.posts = this.posts.concat(data.articles);
+          //this.recyclist.size = data.articles.length;
         }
       });
     }
+
   }
 }
 </script>
